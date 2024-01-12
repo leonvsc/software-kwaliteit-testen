41e6c7023afbc07b13a43b73cc4daff53e66228c
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2024-01-12 20:03:06 +0100

    Changed the test to add a timeout after addin an item to the shopping cart, made sure only one test runs as a time, updated the pipeline

--- gui-testing/shoppingcart.spec.ts
+++ gui-testing/shoppingcart.spec.ts
@@ -1,13 +1,14 @@
 import {test, expect, Page} from '@playwright/test';
+import {deflateRawSync} from "node:zlib";
 
 test('login', async ({ page }) => {
+
   await page.goto('https://demowebshop.tricentis.com/login/');
 
   await page.getByLabel("Email:").fill("n.warnaar@student.avans.nl");
   await page.getByLabel("Password").fill("Hdp64z583!!!d");
   await page.locator(".login-button").click();
-  await page.getByRole('link', { name: 'Shopping cart', exact: true }).click();
-
+  await page.goto('https://demowebshop.tricentis.com/cart/');
   const emptyShoppingCart : boolean = await page.getByText('Your Shopping Cart is empty!', {exact: true}).isVisible();
 
   if (!emptyShoppingCart) {
@@ -21,6 +22,8 @@ test('login', async ({ page }) => {
       .locator(".product-box-add-to-cart-button")
       .click();
 
+  await page.waitForTimeout(1000)
+
   await page.locator("#small-searchterms").fill("Casual Golf Belt")
   await page.locator('.search-box-button').click();
   await page.locator(".item-box")
@@ -28,10 +31,12 @@ test('login', async ({ page }) => {
       .locator(".product-box-add-to-cart-button")
       .click();
 
-  await page.getByRole('link', { name: 'Shopping cart', exact: true }).click();
+  await page.waitForTimeout(1000)
+
+  await page.goto('https://demowebshop.tricentis.com/cart/');
   const test = page.locator("tr")
       .filter({hasText: "Total:"})
-      .locator(".order-total").filter({hasText: "101.00"});
+      .locator(".order-total");
 
   await expect(test).toContainText("101.00")
 
