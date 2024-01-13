9f3347ba22b801399462fa9f3e396a8796c6e12f
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2024-01-13 21:45:00 +0100

    Code voor gui testen met pipeline en uitbereiding main pipeline (#7)
    * Created ui tests for adding and removing items from the shopping cart
    
    * Changed the github actions files, so the pipeline will run at push of this branch
    
    * Changed the folder the steps need to run in
    
    * Changed the name of the testfile, improved the tests because they are breaking on github, removed unused code
    
    * --amend
    
    * Added code for saucelabs
    
    * Added a .sauceignore file to fix an error regarding double node modules
    
    * Added steps per browser sort, to fix login/shoppingcart issues
    
    * Fixed a typo in the file and added the saucelabs test
    
    * Changed the pipeline to include the saucelabs credentials
    
    * Fixed a mistake is aligning the code in the pipeline
    
    * Changed the pipeline to include the correct dir it needs to run in
    
    * Changed the way the working dir is set, due to an error
    
    * Moved the .sauce dir to check if this will resolve a directory issue
    
    * Renamed the files to match the correct location
    
    * Moved the files to make sure they can be accessed by the pipeline
    
    * Changed the config file to access the correct files
    
    * Changed the test to add a timeout after addin an item to the shopping cart, made sure only one test runs as a time, updated the pipeline
    
    * Changed the path on the report file to make sure an artifect can be uploaded
    
    * Changed the name of the gui-tests, added the playwright pipeline to the main one, added some more timeout
    
    * Fixed a mistake in the naming
    
    * Upped the version on the actions to 4
    
    * Changed the version of the saucelabs code due to an error
    
    * Update pipeline-qa.yml
    
    Van ** push branch terug naar main gezet.
    
    ---------
    
    Co-authored-by: Boogieguy4 <89997608+Boogieguy4@users.noreply.github.com>

---
+++ playwright.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+
+// Local reporter configuration.
+let reporter = [['html', { open: 'never' }]];
+
+// Tweak reporter when running in Sauce Labs.
+if (process.env.SAUCE_VM) {
+  reporter = [['html', { open: 'never'}]];
+}
+
+export default defineConfig({
+  testDir: './gui-tests',
+  /* Run gui-tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 3 : 0,
+  /* Opt out of parallel gui-tests on CI. */
+  workers: process.env.CI ? 1 : 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
