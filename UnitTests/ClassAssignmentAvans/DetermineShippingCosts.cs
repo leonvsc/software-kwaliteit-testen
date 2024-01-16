ee0b2d5f0aedc80e75ea4915018c2075ec073f23
Niels Warnaar <76254458+NielsWarnaar@users.noreply.github.com>
2024-01-16 19:28:02 +0100

    Improved the C# code to reduce nesting and added the files to the gitignore

--- UnitTests/ClassAssignmentAvans/DetermineShippingCosts.cs
+++ UnitTests/ClassAssignmentAvans/DetermineShippingCosts.cs
@@ -5,38 +5,26 @@ public class DetermineShippingCosts
         typeOfShippingCosts, double totalPrice)
     {
         double result = 0;
-        if (calculateShippingCosts == true)
-        {
-            if (totalPrice > 1500)
-            {
-                result = 0;
-            }
-            else
-            {
-                switch (typeOfShippingCosts)
-                {
-                    case "Ground":
-                        result = 100;
-                        break;
-                    case "InStore":
-                        result = 50;
-                        break;
-                    case "NextDayAir":
-                        result = 250;
-                        break;
-                    case "SecondDayAir":
-                        result = 125;
-                        break;
-                    default:
-                        result = 0;
-                        break;
-                }
-            }
-        }
-        else
+        if (calculateShippingCosts == false)
         {
             result = 0;
+            return result;
         }
+
+        if (totalPrice > 1500)
+        {
+            result = 0;
+            return result;
+        }
+
+        result = typeOfShippingCosts switch
+        {
+            "Ground" => 100,
+            "InStore" => 50,
+            "NextDayAir" => 250,
+            "SecondDayAir" => 125,
+            _ => 0
+        };
         return result;
     }
 }
